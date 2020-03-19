265f7c3c65665b0e6ef969e2b5a0eb2aeabb155d
zanomate-desktop <zanomate@gmail.com>
2020-03-19 15:18:37 +0100

    fix missing initI18n export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Localized from './components/Localized'
 import Localization from './components/Localization'
+import initI18n from './helpers/initI18n'
 
 export {
   Localized,
-  Localization
+  Localization,
+  initI18n
 }
